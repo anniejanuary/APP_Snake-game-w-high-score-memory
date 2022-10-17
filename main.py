a01e43ee193a3c644ec0f565f7c717d74af4a4a0
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-10-17 14:06:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,13 +16,12 @@ snake.initial_snake(x=0, y=0)
 target = Target()
 target.random_display()
 game_over = False
-score.all_time_best_score_header()
-score.all_time_best_score_number()
+
 
 def restart(mouseclickx, mouseclicky):
     # why these "mouseclickx, mouseclicky" are needed:
     # https://stackoverflow.com/questions/48869611/turtle-onscreenclick-not-behaving-as-expected
-    score.targets_scored = 0
+    score.current_score = 0
     score.score_turtle.reset()
     score.message.reset()
     score.message.hideturtle()
@@ -44,10 +43,10 @@ def game(game_over):
         snake.move()
         '''If snake eats the target'''
         if snake.snake_body[0].distance(target) < 15: #if the snake head is within 15 pixels of the target
-            score.targets_scored += 1
-            if score.targets_scored > score.all_time_best:
-                score.all_time_best = score.targets_scored
-                score.all_time_best_score_number()
+            score.current_score += 1
+
+            if score.current_score > int(score.high_score): #score.all_time_best:
+                score.update_high_score()
             add_tile_x = snake.snake_body[len(snake.snake_body) - 1].xcor()
             add_tile_y = snake.snake_body[len(snake.snake_body) - 1].ycor()
             add_turtle = Turtle(shape="square")  # instead of seperate: new_turtle.shape("square")
@@ -57,14 +56,14 @@ def game(game_over):
             snake.snake_body.append(add_turtle)
             target.hideturtle()
             target.random_display()
-        score.score()
+        score.update_current_score()
         '''If snake eats itself'''
         for i in range(3, len(snake.snake_body) - 1): #skipping the head bc that's the 1st collision point, starting with
                                 # tile 4 (index 3) bc thats the first tile the head can reach if it turns around on itself
             if snake.snake_body[0].distance(snake.snake_body[i]) < 15:
                 game_over = True
-                if score.targets_scored > score.all_time_best:
-                    score.all_time_best = score.targets_scored
+                if score.current_score > score.all_time_best:
+                    score.all_time_best = score.current_score
                 score.game_over()
                 '''Restart the game'''
                 screen.onkey(screen.bye, "q")  # Register function exit to event "q" key press
