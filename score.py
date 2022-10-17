03b60d2e5c25029d753a90289c1eeab4980b39de
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-10-17 14:09:45 +0200

    Update score.py

--- score.py
+++ score.py
@@ -1,28 +1,32 @@
 from turtle import Turtle
+
 ALIGNMENT = 'center'
 FONT1 = ('BankGothic Md BT', 15, 'bold') # EG. INSTEAD OF in def __init__(self): | self.style1 = ('Franklin Gothic Book', 15, 'bold')
 FONT2 = ('BankGothic Md BT', 15)
 FONT3 = ('BankGothic Md BT', 17, 'bold')
 
 
-class Score:
+class Score(Turtle):
 
     def __init__(self):
-        self.score_title_turtle = Turtle()
-        self.score_title_turtle.hideturtle()
-        self.score_title_turtle.pu()
-        self.score_title_turtle.goto(x=0, y=280)
-        self.score_title_turtle.color("white")
-        self.score_title_turtle.pd()
-        self.score_title_turtle.write(f"Current score: ", font=FONT1, align=ALIGNMENT)
-        self.score_title_turtle.pu()
+        super().__init__()
+        self.hideturtle()
+        self.pu()
+        self.goto(x=0, y=280)
+        self.color("white")
+        self.pd()
+        self.write(f"Current score: ", font=FONT1, align=ALIGNMENT)
+        self.pu()
+        self.goto(x=0, y=320)
+        self.color("yellow")
+        self.pd()
+        self.write(f"🏆 YOUR BEST SCORE: ", font=FONT3, align=ALIGNMENT)
+        self.pu()
         self.score_turtle = Turtle()
-        self.targets_scored = 0
-        self.all_time_best = 0
-        self.all_time_best_no = Turtle()
-        self.all_time_best_score_shown = Turtle()
+        self.current_score = 0
+        self.display_high_score()
 
-    def score(self):
+    def update_current_score(self):
         self.score_turtle.clear()
         self.score_turtle.reset()
         self.score_turtle.hideturtle()
@@ -30,28 +34,23 @@ class Score:
         self.score_turtle.goto(x=120, y=280)
         self.score_turtle.color("white")
         self.score_turtle.pd()
-        self.score_turtle.write(f"{self.targets_scored}", font=FONT1, align=ALIGNMENT)
+        self.score_turtle.write(f"{self.current_score}", font=FONT1, align=ALIGNMENT)
+        self.score_turtle.pu()
+        self.display_high_score()
+
+    def display_high_score(self):
+        with open("high_score.txt") as high_score_file:
+            self.high_score = high_score_file.read()
+        self.score_turtle.goto(x=170, y=320)
+        self.score_turtle.color("yellow")
+        self.score_turtle.pd()
+        self.score_turtle.write(f"{self.high_score}", font=FONT3, align=ALIGNMENT)
         self.score_turtle.pu()
 
-    def all_time_best_score_header(self):
-        self.all_time_best_score_shown.hideturtle()
-        self.all_time_best_score_shown.pu()
-        self.all_time_best_score_shown.goto(x=0, y=320)
-        self.all_time_best_score_shown.color("yellow")
-        self.all_time_best_score_shown.pd()
-        self.all_time_best_score_shown.write(f"🏆 YOUR BEST SCORE: ", font=FONT3, align=ALIGNMENT)
-        self.all_time_best_score_shown.pu()
-
-    def all_time_best_score_number(self):
-        self.all_time_best_no.clear()
-        self.all_time_best_no.reset()
-        self.all_time_best_no.pu()
-        self.all_time_best_no.goto(x=170, y=320)
-        self.all_time_best_no.color("yellow")
-        self.all_time_best_no.pd()
-        self.all_time_best_no.write(f"{self.all_time_best}", font=FONT3, align=ALIGNMENT)
-        self.all_time_best_no.pu()
-        self.all_time_best_no.hideturtle()
+    def update_high_score(self):
+        with open("high_score.txt", mode="w") as high_score_file:
+            high_score_file.write(str(self.current_score))  # overwrites what's in high_score.txt, if anything
+        self.display_high_score()
 
     def game_over(self):
         self.message = Turtle()
@@ -67,4 +66,3 @@ class Score:
         self.message.pd()
         self.message.write(f"Click anywhere to play again \n       or press 'q' to exit", font=FONT1, align='center')
         self.message.hideturtle()
-
